5012d07bfc218145cc9a2ccad18ac11a9edd53a0
Yajur Vashisht <132369766+yvashisht@users.noreply.github.com>
2024-02-27 19:11:53 -0700

    Initial Set up

---
+++ navigation/MainContainer.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { View } from 'react-native';
+
+import NavigationContainer from @react-navigaton/native
+import createBottomTabNavigator from '@react-navigation/bottom-tabs';
+
+export default function MainContainer() {
+    return(
+        <View>
+            <Text>
+                Hello!
+            </Text>
+        </View>
+    )
+}
